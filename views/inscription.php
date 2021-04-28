e980386e6e29a6828ae265ff3995669d726ba6c8
JoelDeoKL <joeldeocm88@gmail.com>
2021-04-28 21:39:25 +0200

    Mise en place du formulaire

--- views/inscription.php
+++ views/inscription.php
@@ -1,1 +1,15 @@
-<p>Insc</p>
+<form action="#" method="POST">
+    <div>
+        <label>Nom article</label>
+        <input type="text" name="designation" id="designation">
+    </div>
+    <div>
+        <label>Prix unitaire</label>
+        <input type="number" name="prix_unitaire" id="prix_unitaire">
+    </div>
+    <div>
+        <label>Prix total</label>
+        <input type="number" name="prix_total" id="prix_total">
+    </div>
+    <input type="submit" value="Ajouter" name="add_article">
+</form>
