65bb00fdfedba80fc9553d42a4cce7dfaa35cca3
JoelDeoKL <joeldeocm88@gmail.com>
2021-05-02 10:30:27 +0200

    Ajout d'un texte eplicatif aux pages d'authentification

--- views/inscription.php
+++ views/inscription.php
@@ -3,6 +3,12 @@
         <div class="col-lg-12 col-md-12 col-12">
             <form action="#" method="POST">
                 <div class="row">
+                    <div class="col-lg-12 col-md-6 col-12">
+                        <div style="text-align:center">
+                            <h3>Inscrivez-vous</h3>
+                            <p>Passez votre inscription rapidepement en remplissant ce petit formulaire et ayez le droit de publier des citations.<br><br><br><br></p>
+                        </div>
+                    </div>
                     <div class="col-lg-6 col-md-6 col-12">
                         <div class="form-group">
                             <label>Pseudo<span>*</span></label>
